4ddd553de5399b8fdf743fa98f2563d9dbba56b6
EvaSokolyanskaya <esokolyanskaya@lohika.com>
2014-08-21 17:25:41 +0300

    Minor changes

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,10 +1,6 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.ArrayList;
 
 
@@ -14,10 +10,9 @@ public class FileReader {
 		// TODO Auto-generated method stub
 
 		
-		InputStream in = new FileInputStream(new File ("test.txt"));
+		InputStream in = new FileInputStream(new File ("French.txt"));
 				
 		ArrayList<String> Lines = new ArrayList<String>();
-		Byte[] line = new Byte[1024];
 		String newstr = "";
 		
 			int rb;
@@ -27,11 +22,11 @@ public class FileReader {
 				if (rb != '\n') newstr +=(char)rb;
 					else { 
 						Lines.add(newstr);
-						System.out.println(newstr);
+					//	System.out.println(newstr);
 						newstr = "";
 					}
 			}
-			System.out.println(Lines.get(0) + Lines.get(1) + Lines.get(2));
+		//	System.out.println(Lines.get(0) + Lines.get(1) + Lines.get(2));
 			in.close();
 	}
 }
