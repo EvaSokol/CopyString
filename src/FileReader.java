9cef879935dd12718a849bde5533aee21a11d35d
EvaSokol <esokolyanskaya@lohika.com>
2014-08-23 11:17:14 +0300

    part of UI

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,12 +1,8 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-
 
 public class FileReader {
 
---
+++ src/UiFrame.java
@@ -0,0 +1,55 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Panel;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class UiFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		
+		JPanel pan = new JPanel();
+		pan.add(new MyPanel());
+		pan.add(new MyPanel());
+				
+		frame.add(pan);
+				
+		frame.setVisible(true);
+	}
+
+}
+
+class MyPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
+	JPanel pan = new JPanel();
+	
+	MyPanel() {
+	
+		pan.setBackground(Color.GREEN);
+		pan.setSize(300, 200);
+		pan.setLayout(new BorderLayout(5,5));
+		pan.setBounds(10, 10, 200, 300);		
+		
+		JButton display = new JButton("Display");
+		display.setSize(50, 20);
+		pan.add(display, BorderLayout.EAST);
+		JButton copyBtn = new JButton("Copy string");
+		pan.add(copyBtn, BorderLayout.WEST);
+		JButton partBtn = new JButton("Copy part");
+		pan.add(partBtn, BorderLayout.CENTER);
+		pan.setVisible(true);
+		
+		add(pan, BorderLayout.CENTER);
+	}
+	
+}
