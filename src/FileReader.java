872c2d41b7a36913e58e5b7aefcc35c6e3ff3059
EvaSokol <esokolyanskaya@lohika.com>
2014-08-23 08:58:54 +0300

    FileReader works

--- src/FileReader.java
+++ src/FileReader.java
@@ -12,27 +12,19 @@ public class FileReader {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
-		
-		FileInputStream in = new FileInputStream(new File ("French.txt"));
-		InputStreamReader isr = new InputStreamReader(in, "UTF-16");
-		BufferedReader br = new BufferedReader(isr);
-		
-		ArrayList<String> Lines = new ArrayList<String>();
-		String newstr = "";
-		
-			int rb;
+try (
+		FileInputStream in = new FileInputStream(new File ("Chinese.txt"));
+		InputStreamReader isr = new InputStreamReader(in, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);	)	{	
 			
-			while ((rb = in.read()) > 0) {
-				System.out.print((char)rb);
-				if (rb != '\n') newstr +=(char)rb;
-					else { 
-						Lines.add(newstr);
-					//	System.out.println(newstr);
-						newstr = "";
-					}
-			}
-		//	System.out.println(Lines.get(0) + Lines.get(1) + Lines.get(2));
-			in.close();
+			ArrayList<String> Lines = new ArrayList<String>();
+			String str = "";
+							
+			while ((str  = br.readLine()) != null) {
+				Lines.add(str);
+				System.out.println(str);	
+				}
+			
+		}	
 	}
 }
