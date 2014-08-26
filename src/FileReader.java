0e55173d535404585530ea0adfe56aaba911e8a2
EvaSokol <esokolyanskaya@lohika.com>
2014-08-26 23:29:13 +0300

    Copy to buffer works

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,26 +1,57 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 public class FileReader {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-try (
-		FileInputStream in = new FileInputStream(new File ("Chinese.txt"));
-		InputStreamReader isr = new InputStreamReader(in, "UTF-8");
-		BufferedReader br = new BufferedReader(isr);	)	{	
+		
+		ArrayList<String> Lines;
+		
+		Lines = getList(new File ("Chinese.txt"));	
+		System.out.println("There are " + Lines.size() + " strings, max length is " + getMaxString(Lines));
+		}
+
+	public static ArrayList<String> getList(File file) throws IOException,
+			FileNotFoundException, UnsupportedEncodingException {
 			
-			ArrayList<String> Lines = new ArrayList<String>();
-			String str = "";
-							
-			while ((str  = br.readLine()) != null) {
-				Lines.add(str);
-				System.out.println(str);	
+			ArrayList<String> Lines;
+		
+			try (
+				FileInputStream in = new FileInputStream(file);
+				InputStreamReader isr = new InputStreamReader(in, "UTF-8");
+				BufferedReader br = new BufferedReader(isr);	)	{	
+					
+					Lines = new ArrayList<String>();
+					String str = "";
+									
+					while ((str  = br.readLine()) != null) {
+						if (str.length() > 0) { 
+							Lines.add(str);
+							System.out.println(str);
+							}
+						
+						}
+					
 				}
-			
-		}	
+		return Lines;
 	}
+	
+	static int getMaxString(ArrayList<String> list) {
+		int max = 0;
+		for (String string : list) {
+			if (string.length() > max) 
+				max = string.length();
+		}
+		
+		return max;
+	}
+	
+	
 }
--- src/UiFrame.java
+++ src/UiFrame.java
@@ -1,31 +1,50 @@
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class UiFrame {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
-		int StringCount = 20;
-		
+		UiFrame frame = new UiFrame(FileReader.getList(new File("Chinese.txt")));
+	}
+
+	private UiFrame(ArrayList<String> list) {
+				
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(600, 50*StringCount);
+		frame.setSize(600, 400);
 	
 		JPanel pan = new JPanel();
-		pan.setLayout(new GridLayout(StringCount, 1));
+		pan.setLayout(new GridLayout(list.size(), 1));
 				
-		for (int i = 0; i<StringCount; i++)
-			pan.add(new MyPanel(), BorderLayout.AFTER_LAST_LINE);
+		for (String str : list)
+			pan.add(new MyPanel(str), BorderLayout.AFTER_LAST_LINE);
 
 		frame.add(pan);
 		
 				
 		frame.setVisible(true);
+		
 	}
+
+	
 
 }
 
@@ -34,19 +53,70 @@ class MyPanel extends JPanel {
 	
 	JPanel pan = new JPanel();
 	
-	MyPanel() {
+	MyPanel(String str) {
 			
-		JButton display = new JButton("Display01234567890Display");
+		JButton display = new JButton(str);
 		display.setSize(600, 20);
 		display.setEnabled(false);
 		pan.add(display);
-		JButton copyBtn = new JButton("Copy string");
-		pan.add(copyBtn);
-		JButton partBtn = new JButton("Copy part");
-		pan.add(partBtn);
-		pan.setVisible(true);
 		
+		CopyAction listener = new CopyAction(str);
+		
+		JButton copyBtn = new JButton("Copy string");
+		copyBtn.addActionListener(listener);
+		pan.add(copyBtn);
+		
+		JButton partBtn = new JButton("Copy part");
+		partBtn.addActionListener(listener);
+		pan.add(partBtn);
+		
+		pan.setVisible(true);
 		add(pan, BorderLayout.CENTER);
+	}
+	
+}
+
+class CopyAction implements ActionListener {
+
+	String str;
+	
+	CopyAction(String string) {
+		str = string;
+	}
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getActionCommand().compareTo("Copy string") == 0) {
+			String cbst = toBuffer(str);
+			
+			System.out.println("from buffer: " + cbst);
+		}
+		else {
+			
+			String result = "";
+			for (int i=0; i< 5; i++) {
+				result += str.toCharArray()[(int)(Math.random()*str.length())];
+						}
+			System.out.println("Result: " + result);
+		};
+	}
+
+
+	private String toBuffer(String string) throws HeadlessException {
+		
+		StringSelection ss = new StringSelection(string);
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+				 
+		  String cbst = "";
+		try {
+			cbst = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null).getTransferData(Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null).getTransferDataFlavors()[0]);
+		} catch (HeadlessException | UnsupportedFlavorException
+				| IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return cbst;
 	}
 	
 }
